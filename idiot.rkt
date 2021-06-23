69be97a6ea021ae8ee510ca1e671b585a4af8a8b
EvergreenHZ <huaizhixtp@163.com>
2021-06-23 22:57:42 +0800

    still need some beautiful css

--- idiot.rkt
+++ idiot.rkt
@@ -6,13 +6,17 @@
          web-server/dispatch
          )
 
-(define (for-test req)
+#;(define (for-test req)
   (begin
     (displayln "This is For Test")
     (response/xexpr
      '(html
        (head (title "For Test"))
        (body (h1 "This Page is Used For Test"))))))
+
+(define (for-test req)
+  (get-post req "why-i-build-this-web.html"))
+
 (define (http-response content)  ; The 'content' parameter should be a string.
   (response/full
    200                  ; HTTP response code.
@@ -23,12 +27,11 @@
    (list                ; Content (in bytes) to send to the browser.
     (string->bytes/utf-8 content))))
 
+(define (say-hello request name)
+  (http-response (string-append "Hello " name "!")))
+
 (define (get-post request post-name)
-  (http-response (string-append "Hello " post-name "!")))
-#|(response/xexpr
-   '(html
-     (head (title "For Test"))
-     (body (h1 "This Page is Used For Test")))))|#
+  (http-response (file->string (string-append "static/posts/" post-name))))
 
 (define (not-found req)
   (begin
@@ -42,35 +45,31 @@
   (response/output
    (lambda (op) (display (include-template "index.html") op))))
 
-#;(define-values (dispatch url)
-  (dispatch-rules
-   [("idiot.rkt") home-page]
-   [("test.rkt") for-test]
-   [("posts" (post-name)) get-post]
-   [else not-found]))
-
 ;; URL routing table (URL dispatcher).
 (define-values (dispatch generate-url)
   (dispatch-rules
+   [("") not-found]
    [("idiot.rkt") home-page]
    [("test.rkt") for-test]
-   ;;[("posts" (string-arg)) get-post]  ; Notice this line.
-   [else (error "There is no procedure to handle the url.")]))
+   [("hello" (string-arg)) say-hello]
+   [("posts" (string-arg)) get-post]
+   [else not-found]))
+  ;[else (error "There is no procedure to handle the url.")]))
 
 (define (request-handler request)
   (begin
     (displayln request)
     (dispatch request)))
 
-#;(define (start request)
-    (response/output
-     (lambda (op) (display (include-template "index.html") op))))
-
 (serve/servlet request-handler
                #:port 8765
                #:listen-ip #f
-               #:servlet-regexp #rx".*\\.rkt"
                ;;#:servlet-regexp #rx""
+               #:servlet-regexp #rx".*\\.[(rkt)(html)]"
+               ;;#:servlet-regexp #rx".*\\.[(rkt)]"
+               #:server-root-path "./"
+               #:servlets-root	 "./"
+               ;;#:file-not-found-responder "./"
                #:servlet-path "/idiot.rkt"
                #:extra-files-paths (list (build-path "static"))
                #:launch-browser? #t
