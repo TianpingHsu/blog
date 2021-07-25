c5c4a46be8ce87cf18fa9ba4a9cc3416aa3be2f1
EvergreenHZ <huaizhixtp@163.com>
2021-07-25 17:39:49 +0800

    add one column

--- idiot.rkt
+++ idiot.rkt
@@ -9,43 +9,39 @@
 
 (define (http-response content)  ; The 'content' parameter should be a string.
   (response/full
-   200                  ; HTTP response code.
-   #"OK"                ; HTTP response message.
-   (current-seconds)    ; Timestamp.
-   TEXT/HTML-MIME-TYPE  ; MIME type for content.
-   '()                  ; Additional HTTP headers.
-   (list                ; Content (in bytes) to send to the browser.
-    (string->bytes/utf-8 content))))
+    200                  ; HTTP response code.
+    #"OK"                ; HTTP response message.
+    (current-seconds)    ; Timestamp.
+    TEXT/HTML-MIME-TYPE  ; MIME type for content.
+    '()                  ; Additional HTTP headers.
+    (list                ; Content (in bytes) to send to the browser.
+      (string->bytes/utf-8 content))))
 
-(define (get-post request post-name)
+(define (get-post req post-name)
   (let ([file-name (string-append "static/posts/" post-name)])
     (if (file-exists? file-name)
-        (http-response (file->string file-name))
-        not-found)))
-
-(define (for-test req)
-  (get-post req "why-i-build-this-web.html"))
+      (http-response (file->string file-name))
+      not-found)))
 
 (define (not-found req)
   (begin
     (displayln "In Handler: not-found")
     (response/xexpr
-     '(html
-       (head (title "Page Not Found"))
-       (body (h1 "You Are In My Error Handler: *Page Not Found*, Check Your URL"))))))
+      '(html
+         (head (title "Page Not Found"))
+         (body (h1 "You Are In My Error Handler: *Page Not Found*, Check Your URL"))))))
 
 (define (home-page req)
   (response/output
-   (lambda (op) (display (include-template "index.html") op))))
+    (lambda (op) (display (include-template "index.html") op))))
 
 ;; URL routing table (URL dispatcher).
 (define-values (dispatch request)
   (dispatch-rules
-   [("idiot.rkt") home-page]
-   [("test.rkt") for-test]
-   [("posts" (string-arg)) get-post]
-   [else not-found]))
-  ;[else (error "There is no procedure to handle the url.")]))
+    [("idiot.rkt") home-page]
+    [("posts" (string-arg)) get-post]
+    [else not-found]))
+;[else (error "There is no procedure to handle the url.")]))
 
 (define (request-handler request)
   (begin
